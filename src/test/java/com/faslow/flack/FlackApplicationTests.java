bc8efca6c75aeae776e2bb1aa7db9ba30b61de3b
dksalsgus <alsgus2ek@naver.com>
2021-04-06 23:06:45 +0900

    set

---
+++ src/test/java/com/faslow/flack/FlackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faslow.flack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
