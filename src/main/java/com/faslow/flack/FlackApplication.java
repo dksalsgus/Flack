f6174ced01b36bc2c80cddd12677629a7fca4b23
dksalsgus <alsgus2ek@naver.com>
2021-04-11 00:46:43 +0900

    UserController Set

--- src/main/java/com/faslow/flack/FlackApplication.java
+++ src/main/java/com/faslow/flack/FlackApplication.java
@@ -2,12 +2,14 @@ package com.faslow.flack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class FlackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FlackApplication.class, args);
+    }
 
 }
