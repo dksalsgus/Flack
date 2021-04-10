fd6607feefc5c1d18b236657583bcf97c589f5e6
dksalsgus <alsgus2ek@naver.com>
2021-04-11 00:47:30 +0900

    UserService Set

--- src/main/java/com/faslow/flack/config/principal/UserDetailService.java
+++ src/main/java/com/faslow/flack/config/principal/UserDetailService.java
@@ -1,15 +1,28 @@
 package com.faslow.flack.config.principal;
 
+import com.faslow.flack.entity.user.User;
+import com.faslow.flack.repository.UserRepository;
+import javassist.NotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User findUser = null;
+        try {
+            findUser = userRepository.findByuserEmail(username).orElseThrow(() -> new NotFoundException("Not Found User"));
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        return new UserPrincipal(findUser);
     }
 }
---
+++ src/main/java/com/faslow/flack/service/UserService.java
@@ -0,0 +1,12 @@
+package com.faslow.flack.service;
+
+import com.faslow.flack.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+}
