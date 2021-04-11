0d08f2c5abd23512694217e65b42ae8c54380df0
ooiooc <shinjh1102@naver.com>
2021-04-12 00:22:30 +0900

    feat : 회원가입 추가

--- src/main/java/com/faslow/flack/service/UserService.java
+++ src/main/java/com/faslow/flack/service/UserService.java
@@ -1,12 +1,29 @@
 package com.faslow.flack.service;
 
+import com.faslow.flack.dto.UserDto;
+import com.faslow.flack.entity.user.User;
 import com.faslow.flack.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void join(UserDto userDto){
+        if (userRepository.findByuserEmail(userDto.getUserEmail()).orElse(null) != null) {
+            throw new RuntimeException("가입된 회원입니다.");
+        }
+        User user = userDto.toEntity();
+        userDto.setUserPw(passwordEncoder.encode(userDto.getUserPw()));
+        userRepository.save(user);
+    }
+
 }
