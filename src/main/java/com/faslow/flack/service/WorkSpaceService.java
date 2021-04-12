9046bf59c4045b25d1d20939acfd3135811aa5e3
dksalsgus <alsgus2ek@naver.com>
2021-04-12 22:12:48 +0900

    WorkSpace Service Set

--- src/main/java/com/faslow/flack/service/WorkSpaceService.java
+++ src/main/java/com/faslow/flack/service/WorkSpaceService.java
@@ -1,13 +1,23 @@
 package com.faslow.flack.service;
 
+import com.faslow.flack.entity.workspace.WorkSpace;
+import com.faslow.flack.entity.workspace.WorkSpaceCreateRequest;
 import com.faslow.flack.repository.WorkSpaceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class WorkSpaceService {
 
     private final WorkSpaceRepository workSpaceRepository;
+
+    @Transactional
+    public WorkSpace createWorkSpace(WorkSpaceCreateRequest workSpaceCreateRequest) {
+        return workSpaceRepository.save(
+                new WorkSpace(workSpaceCreateRequest.getWorkspaceName(), workSpaceCreateRequest.getChannelNo(), workSpaceCreateRequest.getWorkspaceRole()));
+    }
 
 }
