53512132aa1d4df47ffd8583d837184865e66ba7
안민현 <alsgus2ek@naver.com>
2021-04-12 14:14:55 +0900

    GroupService 추가

---
+++ src/main/java/com/faslow/flack/service/GroupService.java
@@ -0,0 +1,13 @@
+package com.faslow.flack.service;
+
+import com.faslow.flack.repository.GroupRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GroupService {
+
+    private final GroupRepository groupRepository;
+
+}
