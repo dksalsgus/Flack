834757c90800c33a7724daa2dd046c732c052f96
ooiooc <shinjh1102@naver.com>
2021-04-13 06:15:36 +0900

    fix : 회원가입 test

--- src/test/java/com/faslow/flack/UserServiceTest.java
+++ src/test/java/com/faslow/flack/UserServiceTest.java
@@ -1,25 +1,42 @@
 package com.faslow.flack;
 
-import com.faslow.flack.entity.user.User;
-import com.faslow.flack.repository.UserRepository;
-import org.aspectj.lang.annotation.After;
+import com.faslow.flack.dto.UserDto;
+import com.faslow.flack.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class UserServiceTest {
 
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    String userEmail = "email test";
+    String userPw = "userpw test";
+    String userPhone = "000-0000-0000";
 
     @Test
-    public void join(){
-        userRepository.save(User.builder()
-                .userEmail("email test")
-                .userPw("pw test")
-                .userPhone("010-0000-0000")
-                .build());
+    public void 회원가입() throws Exception{
 
-        }
+        UserDto userDto = new UserDto();
+        userDto.setUserEmail(userEmail);
+        userDto.setUserPw(userPw);
+        userDto.setUserPhone(userPhone);
+
+        userService.join(userDto);
+
+        //assertj : 테스트 검증 라이브러리의 검증 메소드
+        //isEqualTo : assertj의 비교 메소드, 값이 같을 때만 성공
+
+        assertThat(userDto.getUserEmail()).isEqualTo(userEmail);
+        assertThat(passwordEncoder.matches(userPw, userDto.getUserPw()));
+        assertThat(userDto.getUserPhone()).isEqualTo(userPhone);
+
+    }
 }
