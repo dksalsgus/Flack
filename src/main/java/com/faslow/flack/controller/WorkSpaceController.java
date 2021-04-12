5ada3027857c5de047c4d4109c2e412ca2ecb92e
dksalsgus <alsgus2ek@naver.com>
2021-04-12 22:12:33 +0900

    WorkSpace Contoller Set

--- src/main/java/com/faslow/flack/controller/WorkSpaceController.java
+++ src/main/java/com/faslow/flack/controller/WorkSpaceController.java
@@ -1,14 +1,27 @@
 package com.faslow.flack.controller;
 
+import com.faslow.flack.entity.workspace.WorkSpace;
+import com.faslow.flack.entity.workspace.WorkSpaceCreateRequest;
 import com.faslow.flack.service.WorkSpaceService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@Api(value = "그룹 컨트롤러")
+@Api(value = "워크스페이스 api")
 public class WorkSpaceController {
 
     private final WorkSpaceService workSpaceService;
+
+    @ApiOperation(value = "워크 스페이스 생성")
+    @PostMapping("workspace")
+    public ResponseEntity<String> createWorkSpace(@RequestBody WorkSpaceCreateRequest workSpaceCreateRequest) {
+        WorkSpace saveWorkSpace = workSpaceService.createWorkSpace(workSpaceCreateRequest);
+        return ResponseEntity.ok(saveWorkSpace.getWorkspaceName());
+    }
 }
