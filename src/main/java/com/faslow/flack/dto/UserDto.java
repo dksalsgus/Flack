dd1599c465eb0a387ffcdb4ac771e94daa8b98b7
ooiooc <shinjh1102@naver.com>
2021-04-13 05:32:03 +0900

    chore : entity 삭제

--- src/main/java/com/faslow/flack/dto/UserDto.java
+++ src/main/java/com/faslow/flack/dto/UserDto.java
@@ -10,7 +10,6 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class UserDto {
 
-    private Long userNo;
     private String userEmail;
     private String userPw;
     private String userPhone;
@@ -20,7 +19,6 @@ public class UserDto {
     // toEntity() 메서드를 통해 dto에서 필요한 부분을 이용하여 entity로 변환
     public User toEntity(){
         return User.builder()
-                .userNo(userNo)
                 .userEmail(userEmail)
                 .userPw(new BCryptPasswordEncoder().encode(userPw))
                 .userPhone(userPhone)
@@ -28,8 +26,7 @@ public class UserDto {
     }
 
     @Builder
-    public UserDto(Long userNo, String userEmail, String userPw, String userPhone) {
-        this.userNo = userNo;
+    public UserDto(String userEmail, String userPw, String userPhone) {
         this.userEmail = userEmail;
         this.userPw = userPw;
         this.userPhone = userPhone;
