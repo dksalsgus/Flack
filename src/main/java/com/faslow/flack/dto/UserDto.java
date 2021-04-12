c87ee38c0d9775b904763cd2962c66cd17c1dc56
ooiooc <shinjh1102@naver.com>
2021-04-12 12:48:59 +0900

    userDto 생성

--- src/main/java/com/faslow/flack/dto/UserDto.java
+++ src/main/java/com/faslow/flack/dto/UserDto.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import java.time.LocalDateTime;
 
 @Data
@@ -16,28 +15,25 @@ public class UserDto {
     private String userEmail;
     private String userPw;
     private String userPhone;
-    private String role;
     private LocalDateTime createAt;
     private LocalDateTime updateAt;
 
-    // toEntity() 메서드를 통해 dto에서 필요한 부분을 이용하여 entity로 만든다
+    // toEntity() 메서드를 통해 dto에서 필요한 부분을 이용하여 entity로 변환
     public User toEntity(){
         return User.builder()
                 .userNo(userNo)
                 .userEmail(userEmail)
                 .userPw(new BCryptPasswordEncoder().encode(userPw))
                 .userPhone(userPhone)
-                .role(role)
                 .build();
     }
 
     @Builder
-    public UserDto(Long userNo, String userEmail, String userPw, String userPhone, String role) {
+    public UserDto(Long userNo, String userEmail, String userPw, String userPhone) {
         this.userNo = userNo;
         this.userEmail = userEmail;
         this.userPw = userPw;
         this.userPhone = userPhone;
-        this.role = role;
     }
 
 }
