c058bf64c989c0883d7fb3e9474bcd2c3a5f274e
dksalsgus <alsgus2ek@naver.com>
2021-04-13 23:50:43 +0900

    feat : user update controller

--- src/main/java/com/faslow/flack/controller/UserController.java
+++ src/main/java/com/faslow/flack/controller/UserController.java
@@ -1,15 +1,15 @@
 package com.faslow.flack.controller;
 
 import com.faslow.flack.entity.dto.user.UserDto;
+import com.faslow.flack.entity.dto.user.UserUpdateRequest;
 import com.faslow.flack.entity.user.User;
 import com.faslow.flack.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,4 +25,12 @@ public class UserController {
         User saveUser = userService.join(userDto);
         return ResponseEntity.ok(new UserDto(saveUser));
     }
+
+    @PatchMapping("user/{userId}")
+    @ApiOperation(value = "회원 정보 수정")
+    public ResponseEntity<UserDto> updateUser(@PathVariable Long userId, @RequestBody UserUpdateRequest userUpdateRequest) throws NotFoundException {
+        User updateUser = userService.update(userId, userUpdateRequest);
+        return ResponseEntity.ok(new UserDto(updateUser));
+    }
+
 }
