0243d30568d296a62c7e11d1f179afddaa7a553c
안민현 <alsgus2ek@naver.com>
2021-04-16 11:54:34 +0900

    feat : 프로필 조회 service

--- src/main/java/com/faslow/flack/service/ProfileService.java
+++ src/main/java/com/faslow/flack/service/ProfileService.java
@@ -3,6 +3,7 @@ package com.faslow.flack.service;
 import com.faslow.flack.entity.dto.profile.ProfileDto;
 import com.faslow.flack.entity.profile.Profile;
 import com.faslow.flack.repository.ProfileRepository;
+import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,11 @@ public class ProfileService {
     @Transactional
     public Profile createProfile(ProfileDto profileDto) {
         return profileRepository.save(new Profile(profileDto.getProfileName(), profileDto.getProfileState(), profileDto.getProfilePicture(), profileDto.getUserNo()));
+    }
+
+    @Transactional
+    public Profile profileDetails(Long profileNo) throws NotFoundException {
+        return profileRepository.findById(profileNo).orElseThrow(() -> new NotFoundException("Not Found Profile"));
     }
 
 }
