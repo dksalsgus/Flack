af9d07994104ba60e29cd463b171974aebad751c
ooiooc <shinjh1102@naver.com>
2021-04-20 06:02:34 +0900

    test : delete WorkSpace

--- src/test/java/com/faslow/flack/service/WorkSpaceServiceTest.java
+++ src/test/java/com/faslow/flack/service/WorkSpaceServiceTest.java
@@ -1,13 +1,15 @@
 package com.faslow.flack.service;
 
 import com.faslow.flack.entity.dto.workspace.WorkSpaceCreateRequest;
+import com.faslow.flack.entity.user.User;
 import com.faslow.flack.entity.workspace.WorkSpace;
+import com.faslow.flack.repository.WorkSpaceRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -20,11 +22,18 @@ class WorkSpaceServiceTest {
     @Autowired
     WorkSpaceService workSpaceService;
 
+    @Autowired
+    WorkSpaceRepository workSpaceRepository;
+
     private String workspaceName;
+    private Long workspaceNo;
+
 
     @BeforeAll
     void setUp() {
+        workspaceNo = 1L;
         workspaceName = "faslow";
+
     }
 
     @Test
@@ -44,6 +53,14 @@ class WorkSpaceServiceTest {
         for (WorkSpace i : workSpaceList) {
             log.info("WorkSpace List : {}", i.getWorkspaceName());
         }
+    }
+
+    @Test
+    @Order(3)
+    void 워크스페이스_삭제(){
+        WorkSpace workSpace = workSpaceService.deleteWorkSpace(workspaceNo);
+        assertThat(workSpace).isNull();
+        log.info("Deleted WorkSpace : ", workSpace);
     }
 
 }
