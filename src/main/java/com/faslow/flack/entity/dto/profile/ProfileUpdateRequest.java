ca633f64d964451dbed9b600def8d41c407cf3e7
ooiooc <shinjh1102@naver.com>
2021-04-21 08:09:58 +0900

    refactor : profile update request dto 수정

--- src/main/java/com/faslow/flack/entity/dto/profile/ProfileUpdateRequest.java
+++ src/main/java/com/faslow/flack/entity/dto/profile/ProfileUpdateRequest.java
@@ -1,6 +1,8 @@
 package com.faslow.flack.entity.dto.profile;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,6 +10,8 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@ApiModel(value = "프로필 편집")
+@AllArgsConstructor
 public class ProfileUpdateRequest {
 
     @ApiModelProperty(value = "프로필 이름")
