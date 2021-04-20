4945d76fa027ec2d3ddf4f410fbfc67df65c5162
ooiooc <shinjh1102@naver.com>
2021-04-21 08:03:07 +0900

    add : 회원정보수정 dto 생성

---
+++ src/main/java/com/faslow/flack/entity/dto/profile/ProfileUpdateRequest.java
@@ -0,0 +1,22 @@
+package com.faslow.flack.entity.dto.profile;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ProfileUpdateRequest {
+
+    @ApiModelProperty(value = "프로필 이름")
+    private String profileName;
+
+    @ApiModelProperty(value = "프로필 상태메세지")
+    private String profileState;
+
+    @ApiModelProperty(value = "프로필 사진 경로")
+    private String profilePicture;
+
+}
