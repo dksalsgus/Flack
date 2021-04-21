e8b3a21a9b3a1dd8a88022a79c4dbc676e6d2ccb
ooiooc <shinjh1102@naver.com>
2021-04-21 21:27:27 +0900

    feat : update profile service

--- src/main/java/com/faslow/flack/service/ProfileService.java
+++ src/main/java/com/faslow/flack/service/ProfileService.java
@@ -1,11 +1,11 @@
 package com.faslow.flack.service;
 
 import com.faslow.flack.entity.dto.profile.ProfileDto;
+import com.faslow.flack.entity.dto.profile.ProfileUpdateRequest;
 import com.faslow.flack.entity.profile.Profile;
 import com.faslow.flack.repository.ProfileRepository;
 import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -24,6 +24,13 @@ public class ProfileService {
     @Transactional
     public Profile profileDetails(Long profileNo) throws NotFoundException {
         return profileRepository.findById(profileNo).orElseThrow(() -> new NotFoundException("Not Found Profile"));
+    }
+
+    @Transactional
+    public Profile updateProfile(Long profileNo, ProfileUpdateRequest profileUpdateRequest) throws  NotFoundException{
+        Profile profile = profileRepository.findById(profileNo).orElseThrow(() -> new NotFoundException("Not Found Profile"));
+        profile.update(profileUpdateRequest.getProfileName(), profileUpdateRequest.getProfileState(), profileUpdateRequest.getProfileState());
+        return profile;
     }
 
 }
