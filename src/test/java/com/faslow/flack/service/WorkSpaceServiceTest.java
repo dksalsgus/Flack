0182e388a69d719b50f3aa526fd196bf27e5a0a5
안민현 <alsgus2ek@naver.com>
2021-04-21 09:46:38 +0900

    TestCode 수정

--- src/test/java/com/faslow/flack/service/WorkSpaceServiceTest.java
+++ src/test/java/com/faslow/flack/service/WorkSpaceServiceTest.java
@@ -6,13 +6,13 @@ import com.faslow.flack.entity.user.User;
 import com.faslow.flack.entity.workspace.WorkSpace;
 import com.faslow.flack.repository.UserRepository;
 import com.faslow.flack.repository.UserWorkRepository;
+import com.faslow.flack.repository.WorkSpaceRepository;
 import javassist.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -25,6 +25,9 @@ class WorkSpaceServiceTest {
 
     @Autowired
     WorkSpaceService workSpaceService;
+
+    @Autowired
+    WorkSpaceRepository workSpaceRepository;
 
     @Autowired
     UserRepository userRepository;
@@ -40,18 +43,16 @@ class WorkSpaceServiceTest {
     void setUp() {
         workspaceNo = 1L;
         workspaceName = "faslow";
-
         userRepository.save(new User("userEmail", "userPw", "010-1234-1234"));
     }
 
     @Test
     @Order(1)
-    @Transactional
     void 워크스페이스_생성() throws NotFoundException {
         WorkSpaceCreateRequest workspace = new WorkSpaceCreateRequest(this.workspaceName, 0);
         WorkSpace savedWorkSpace = workSpaceService.createWorkSpace("userEmail", workspace);
         assertThat(savedWorkSpace.getWorkspaceName()).isEqualTo(workspaceName);
-        log.info("Created workSpace Name : {}", savedWorkSpace.getWorkspaceName());
+        log.info("Created workSpace Name : {}", savedWorkSpace);
         List<UserWorkSpace> findWorkSpaces = userWorkRepository.findAllByWorkSpace(savedWorkSpace);
         log.info("find workspace : {}", findWorkSpaces);
     }
@@ -61,18 +62,15 @@ class WorkSpaceServiceTest {
     void 워크스페이스_리스트() {
         List<WorkSpace> workSpaceList = workSpaceService.listWorkSpace();
         assertThat(workSpaceList).isNotNull();
-        for (WorkSpace i : workSpaceList) {
-            log.info("WorkSpace List : {}", i.getWorkspaceName());
-        }
+        log.info("WorkSpaceList : {}", workSpaceList);
     }
 
     @Test
     @Order(3)
-    @Transactional
     void 워크스페이스_삭제() {
         userWorkRepository.deleteById(1L);
         WorkSpace workSpace = workSpaceService.deleteWorkSpace(workspaceNo);
-        assertThat(workSpace).isNull();
+        assertThat(workSpace.getWorkspaceName()).isEqualTo("faslow"); // findById의 결과라 Null이 아님
         log.info("Deleted WorkSpace : ", workSpace);
     }
 
