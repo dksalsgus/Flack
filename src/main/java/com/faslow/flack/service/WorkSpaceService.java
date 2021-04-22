3972d5e6c6a5462553c757d3f833c40f9e51421b
ooiooc <shinjh1102@naver.com>
2021-04-23 08:02:53 +0900

    feat : workspace 삭제 시 profile도 함께 삭제되도록 profile delete service 수정

--- src/main/java/com/faslow/flack/service/WorkSpaceService.java
+++ src/main/java/com/faslow/flack/service/WorkSpaceService.java
@@ -1,12 +1,15 @@
 package com.faslow.flack.service;
 
 import com.faslow.flack.entity.dto.workspace.WorkSpaceCreateRequest;
+import com.faslow.flack.entity.profile.Profile;
 import com.faslow.flack.entity.user.User;
 import com.faslow.flack.entity.workspace.WorkSpace;
+import com.faslow.flack.repository.ProfileRepository;
 import com.faslow.flack.repository.UserRepository;
 import com.faslow.flack.repository.WorkSpaceRepository;
 import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +24,8 @@ public class WorkSpaceService {
     private final UserWorkSpaceService userWorkSpaceService;
 
     private final UserRepository userRepository;
+
+    private final ProfileRepository profileRepository;
 
 
     @Transactional(readOnly = true)
@@ -40,11 +45,13 @@ public class WorkSpaceService {
     }
 
     @Transactional(readOnly = true)
-    public WorkSpace deleteWorkSpace(Long workspaceNo) {
-        WorkSpace workSpace = workSpaceRepository.findById(workspaceNo)
-                .orElseThrow(() -> new IllegalArgumentException("Not Found WorkSpace" + workspaceNo));
-        workSpaceRepository.delete(workSpace);
-        return workSpace;
+    public void deleteWorkSpace(Long workspaceNo, Long profileNo) throws NotFoundException {
+        try {
+            workSpaceRepository.deleteById(workspaceNo);
+            profileRepository.deleteById(profileNo);
+        } catch (EmptyResultDataAccessException e) {
+            throw new NotFoundException(e.getMessage());
+        }
     }
 
 }
